fda9d865b458c5fb4754eadb3fc82d66be8c98c4
Ainur Latypov <ainurlatypov@gmail.com>
2017-06-07 12:56:30 +0300

    add project

---
+++ MobileCenterDemoApp.UITests/AppInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MobileCenterDemoApp.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile("../../../MobileCenterDemoApp/MobileCenterDemoApp.Android/bin/Debug/" +
+                    "com.mobilecenterdemoapp.xamarin-Signed.apk")
+                    .DeviceSerial("91b7d68a")
+                    .StartApp();
+            }
+
+            if (platform == Platform.iOS)
+            {
+                return ConfigureApp
+                    .iOS
+                    .StartApp();
+            }
+
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
+
